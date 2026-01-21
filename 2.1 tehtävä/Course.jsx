bc60f7541fb8cb403dea6eb089e61d87b79e2342
onni <holttaonni@gmail.com>
2026-01-21 09:14:25 +0200

    Add files via upload

---
+++ 2.1 tehtävä/Course.jsx
@@ -0,0 +1,25 @@
+const Header = ({ course }) => (
+    <h1>{course.Header}</h1>
+)
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Course = ({ course }) => (
+  <div>
+   <Header course={course} />
+   <Content parts={course.parts} />
+  </div>
+)
+export default Course
