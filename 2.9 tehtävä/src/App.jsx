4d09d1adf05f1cc9ef06e6ce579c42ef734424c8
onni <holttaonni@gmail.com>
2026-01-26 08:44:45 +0200

    Add files via upload

---
+++ 2.9 tehtävä/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingName = persons.find(
+      person => person.name === newName
+    )
+
+    if (existingName) {
+      window.alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+ return (
+    <div>
+      <h2>Phonebook</h2>
+        <div>filter shown with<input value={filter} onChange={handleFilterChange} /></div>
+      <h2>Add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div><button type="submit">add</button></div>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>
+        
+        {personsToShow.map((person, index) => (
+          <li key={index}>{person.name} {person.number}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
