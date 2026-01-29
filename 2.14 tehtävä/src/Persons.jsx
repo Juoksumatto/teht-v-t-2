b6db73db7fb34849d469eb1c504630de79064564
onni <holttaonni@gmail.com>
2026-01-29 10:45:08 +0200

    Add files via upload

---
+++ 2.14 tehtävä/src/Persons.jsx
@@ -0,0 +1,14 @@
+const Persons = ({ persons, removePerson }) => (
+  <ul>
+    {persons.map(person => (
+      <li key={person.id}>
+        {person.name} {person.number}
+        <button onClick={() => removePerson(person.id)}>
+          delete
+        </button>
+      </li>
+    ))}
+  </ul>
+)
+
+export default Persons
