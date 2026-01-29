58b950058cba45eadc26c1f1ed310de0643913a3
onni <holttaonni@gmail.com>
2026-01-29 08:58:33 +0200

    Add files via upload

---
+++ 2.13 tehtävä/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react'
+import Filter from './Filter'
+import Person from './Person'
+import Persons from './Persons'
+import personService from './services/service'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingPerson = persons.find(
+      person => person.name === newName
+    )
+
+    if (existingPerson) {
+      window.alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    personService
+      .create(personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter
+        filter={filter}
+        handleFilterChange={(e) => setFilter(e.target.value)}
+      />
+
+      <h2>Add a new</h2>
+      <Person
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={(e) => setNewName(e.target.value)}
+        newNumber={newNumber}
+        handleNumberChange={(e) => setNewNumber(e.target.value)}
+      />
+
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
