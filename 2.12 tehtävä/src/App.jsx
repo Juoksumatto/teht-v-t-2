13d066eeeb3b8e0e32f22fe8a477a48215dd1ed9
onni <holttaonni@gmail.com>
2026-01-29 08:23:14 +0200

    Add files via upload

---
+++ 2.12 tehtävä/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './Filter'
+import Person from './Person'
+import Persons from './Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingName = persons.find(
+      person => person.name === newName
+    )
+
+    if (existingName) {
+      window.alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(personObject))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+      />
+
+      <h2>Add a new</h2>
+      <Person
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
