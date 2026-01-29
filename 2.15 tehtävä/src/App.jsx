939b38b255b8e647a23d5ae028b9ec25ca74e34b
onni <holttaonni@gmail.com>
2026-01-29 11:39:52 +0200

    Add files via upload

---
+++ 2.15 tehtävä/src/App.jsx
@@ -0,0 +1,117 @@
+import { useState, useEffect } from 'react'
+import Filter from './Filter'
+import Person from './Person'
+import Persons from './Persons'
+import personService from './services/service'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const removePerson = (id) => {
+    const person = persons.find(p => p.id === id)
+
+    if (!window.confirm(`Delete ${person.name}?`)) {
+      return
+    }
+
+    personService
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter(p => p.id !== id))
+    })
+}
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingPerson = persons.find(
+      person => person.name === newName
+    )
+
+    if (existingPerson) {
+      if (existingPerson.number === newNumber) {
+        window.alert(
+          `${newName} is already added to phonebook with the same number.`
+        )
+        return
+      }
+
+      const confirmUpdate = window.confirm(
+        '${newName} is already added to phonebook, replace the old number with a new one?'
+      )
+
+    if (confirmUpdate) {
+      const updatedPerson = {
+        ...existingPerson,
+        number: newNumber
+      }
+
+            personService
+        .update(existingPerson.id, updatedPerson)
+        .then(response => {
+          setPersons(
+            persons.map(p =>
+              p.id !== existingPerson.id ? p : response.data
+            )
+          )
+          setNewName('')
+          setNewNumber('')
+        })
+    }
+    return
+  }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    personService
+      .create(personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+    })
+}
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter
+        filter={filter}
+        handleFilterChange={(e) => setFilter(e.target.value)}
+      />
+
+      <h2>Add a new</h2>
+      <Person
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={(e) => setNewName(e.target.value)}
+        newNumber={newNumber}
+        handleNumberChange={(e) => setNewNumber(e.target.value)}
+      />
+
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} removePerson={removePerson} />
+    </div>
+  )
+}
+
+export default App
